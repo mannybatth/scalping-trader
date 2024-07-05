3fd615bad59418f1a2db0b7579c74780574fcf32
Manny Singh <likebeats@gmail.com>
2024-07-05 19:32:25 -0400

    reset app

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
 import express, { Application, Request, Response } from 'express';
 import fs from 'fs';
 import https from 'https';
-import { TDAmeritrade } from './td/td';
 import nocache from 'nocache';
-import { Alert } from './td/models';
-import { loginToDiscord } from './discord';
 
 const app: Application = express();
-const port = 3000;
-
-const td = new TDAmeritrade();
-// const binance = new BinanceClient();
-
-loginToDiscord(td, () => {});
+const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,88 +14,6 @@ app.get('/', (req: Request, res: Response): Response => {
     return res.status(200).send({
         message: 'Hello World!'
     });
-});
-
-app.get('/td-callback', async (req: Request, res: Response) => {
-    console.log('td callback called');
-    try {
-        const response = await td.createAccessToken(req.query?.code as string);
-        return res.status(200).send(response);
-    } catch (e: any) {
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
-});
-
-app.get('/td/login', (req: Request, res: Response) => {
-    return res.redirect(301, td.authUrl);
-});
-
-app.get('/td/accounts', async (req: Request, res: Response) => {
-    try {
-        const response = await td.getAccounts();
-        return res.status(200).send(response);
-    } catch (e: any) {
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
-});
-
-app.get('/td/get-options-chain', async (req: Request, res: Response) => {
-    try {
-        const response = await td.getOptionsChain(req.query.symbol as string);
-        return res.status(200).send(response);
-    } catch (e: any) {
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
-});
-
-app.get('/td/get-options-chain', async (req: Request, res: Response) => {
-    try {
-        const response = await td.getOptionsChain(req.query.symbol as string);
-        return res.status(200).send(response);
-    } catch (e: any) {
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
-});
-
-app.get('/td/streamer-subscription-keys', async (req: Request, res: Response) => {
-    try {
-        const response = await td.getSubscriptionKeys();
-        return res.status(200).send(response);
-    } catch (e: any) {
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
-});
-
-app.post('/alert', async (req: Request, res: Response) => {
-    try {
-        const alert = req.body as Alert;
-        if (alert.crypto) {
-            // const response = await binance.processAlert(alert);
-            // return res.status(200).send(response);
-            return res.status(200).send();
-        } else {
-            const response = await td.processAlert(alert);
-            return res.status(200).send(response);
-        }
-    }
-    catch (e: any) {
-        console.log({
-            error: e.message || e
-        });
-        return res.status(200).send({
-            error: e?.message || e
-        });
-    }
 });
 
 try {
